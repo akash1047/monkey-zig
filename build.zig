537752a76daf5f3e78c26183c9dcd9bd9d4fd37b
Akash Lohar <94453657+akash1047@users.noreply.github.com>
2023-08-15 16:50:08 +0530

    change dynamic lib dir

--- build.zig
+++ build.zig
@@ -12,7 +12,7 @@ pub fn build(b: *std.build.Builder) void {
     const mode = b.standardReleaseOptions();
 
     const exe = b.addExecutable("monkey-zig", "src/main.zig");
-    exe.addLibraryPath("repel/target/release");
+    exe.addLibraryPath("lib");
     exe.linkSystemLibrary("repel");
     exe.linkLibC();
     exe.setTarget(target);
